8d1051fe48112f418e6724441e8ca757444e1212
Annibale Menolascina <annibale.menolascina@gmail.com>
2025-03-18 10:36:27 +0100

    First Entity Framework Migration

---
+++ VitoSwimPT.Server/Migrations/20250318093050_InitialSwimDB.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VitoSwimPT.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialSwimDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Esercizi",
+                columns: table => new
+                {
+                    EsercizioId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Ripetizioni = table.Column<int>(type: "int", nullable: false),
+                    Distanza = table.Column<int>(type: "int", nullable: false),
+                    Recupero = table.Column<int>(type: "int", nullable: false),
+                    Stile = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Esercizi", x => x.EsercizioId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Esercizi");
+        }
+    }
+}
