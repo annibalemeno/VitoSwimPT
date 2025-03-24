093d7905a25d9d1e9774174e05c5c7e401c940e3
Annibale Menolascina <annibale.menolascina@gmail.com>
2025-03-24 10:52:41 +0100

    esercizi component elenco da server

--- vitoswimpt.client/src/app/esercizi/esercizi.component.ts
+++ vitoswimpt.client/src/app/esercizi/esercizi.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+interface Esercizi {
+  ripetizioni: string;
+  distanza: number;
+  recupero: number;
+  stile: string;
+}
 
 @Component({
   selector: 'app-esercizi',
@@ -6,6 +14,26 @@ import { Component } from '@angular/core';
   templateUrl: './esercizi.component.html',
   styleUrl: './esercizi.component.css'
 })
-export class EserciziComponent {
+export class EserciziComponent implements OnInit {
 
+  public esercizi: Esercizi[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getAllenamenti();
+  }
+
+  getAllenamenti() {
+    this.http.get<Esercizi[]>('/esercizi').subscribe(
+      (result) => {
+        this.esercizi = result;
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+
+  title = 'vitoswimpt.client';
 }
