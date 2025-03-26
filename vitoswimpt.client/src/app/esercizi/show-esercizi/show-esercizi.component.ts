27785d850e4f156c4d26d3aba1ed93acd1fffb29
Annibale Menolascina <annibale.menolascina@gmail.com>
2025-03-26 19:03:01 +0100

    show esercizi really clean

--- vitoswimpt.client/src/app/esercizi/show-esercizi/show-esercizi.component.ts
+++ vitoswimpt.client/src/app/esercizi/show-esercizi/show-esercizi.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 interface Esercizi {
+  esercizioId: number;
   ripetizioni: string;
   distanza: number;
   recupero: number;
@@ -18,21 +19,91 @@ export class ShowEserciziComponent implements OnInit {
 
   constructor(private http: HttpClient) { }
 
-  public esercizi: Esercizi[] = [];
+  public EserciziList: Esercizi[] = [];
+  // EserciziList: any = [];
+  ModalTitle = "";
+  ActivateAddEditEsercComp: boolean = false;
+  eserc: any;
 
-    ngOnInit(): void {
-        this.getAllenamenti();
+  Esercizi_Id_Filter = "";
+  Eercizi_Ripetizioni_Filter = "";
+  EserciziListWithoutFilter: any = [];
+
+  ngOnInit(): void {
+        this.refreshEserciziList();
+        //this.getAllenamenti();
+  }
+
+  addClick() {
+    //this.eserc = {
+    //  DepartmentId: "0",
+    //  DepartmentName: ""
+    //}
+    this.ModalTitle = "Add Esercizio";
+    this.ActivateAddEditEsercComp = true;
+  }
+
+  editClick(item: any) {
+    this.eserc = item;
+    this.ModalTitle = "Edit Esercizio";
+    this.ActivateAddEditEsercComp = true;
+  }
+
+  deleteClick(item: any) {
+    if (confirm('Are you sure??')) {
+      //this.service.deleteDepartment(item.DepartmentId).subscribe(data => {
+      //  alert(data.toString());
+      //  this.refreshEserciziList();
+      //})
+      alert('ToDo');
     }
+  }
+
+  closeClick() {
+    this.ActivateAddEditEsercComp = false;
+    this.refreshEserciziList();
+  }
 
 
-  getAllenamenti() {
-    this.http.get<Esercizi[]>('/esercizi').subscribe(
-      (result) => {
-        this.esercizi = result;
-      },
-      (error) => {
-        console.error(error);
+  refreshEserciziList() {
+    this.http.get<Esercizi[]>('/esercizi').subscribe(data => {
+      this.EserciziList = data;
+      this.EserciziListWithoutFilter = data;
+    });
+  }
+
+  sortResult(prop: any, asc: any) {
+    this.EserciziList = this.EserciziListWithoutFilter.sort(function (a: any, b: any) {
+      if (asc) {
+        return (a[prop] > b[prop]) ? 1 : ((a[prop] < b[prop]) ? -1 : 0);
+      }
+      else {
+        return (b[prop] > a[prop]) ? 1 : ((b[prop] < a[prop]) ? -1 : 0);
+      }
+    });
+  }
+
+  FilterFn() {
+    debugger;
+    var DepartmentIdFilter = this.Esercizi_Id_Filter;
+    var DepartmentNameFilter = this.Eercizi_Ripetizioni_Filter;
+
+    this.EserciziList = this.EserciziListWithoutFilter.filter(
+      function (el: any) {
+        return el
       }
     );
   }
+
+
+  //getAllenamenti() {
+  //  this.http.get<Esercizi[]>('/esercizi').subscribe(
+  //    (result) => {
+  //      this.esercizi = result;
+  //    },
+  //    (error) => {
+  //      console.error(error);
+  //    }
+  //  );
+  //}
 }
