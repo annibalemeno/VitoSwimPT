01a8f9bda6874e830c16cc8c4a4352c211640530
Annibale Menolascina <annibale.menolascina@gmail.com>
2025-04-16 11:17:39 +0200

    allenamenti component structure

--- vitoswimpt.client/src/app/app-routing.module.ts
+++ vitoswimpt.client/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EserciziComponent } from './components/esercizi/esercizi.component';
+import { AllenamentiComponent } from './components/allenamenti/allenamenti.component';
 
 const routes: Routes = [
-  { path: 'esercizi', component: EserciziComponent }
+  { path: 'esercizi', component: EserciziComponent },
+  { path: 'allenamenti', component: AllenamentiComponent }
 ];
 
 @NgModule({
---
+++ vitoswimpt.client/src/app/components/allenamenti/allenamenti.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-allenamenti',
+  standalone: false,
+  templateUrl: './allenamenti.component.html',
+  styleUrl: './allenamenti.component.css'
+})
+export class AllenamentiComponent {
+
+}
