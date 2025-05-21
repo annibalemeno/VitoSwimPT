9d59d4394c5de216ad9cd1248222449d268a5abf
Annibale Menolascina <annibale.menolascina@gmail.com>
2025-05-21 17:05:58 +0200

    sorting filtering piani allenamento

--- vitoswimpt.client/src/app/components/piani/show-piani/show-piani.component.ts
+++ vitoswimpt.client/src/app/components/piani/show-piani/show-piani.component.ts
@@ -13,6 +13,12 @@ export class ShowPianiComponent implements OnInit{
   constructor(private service: ApiserviceService) { }
 
   public PianiList: Piani[] = [];
+  PianiIdFilter = "";
+  NomePianoFilter = "";
+  DescrizioneFilter = "";
+  NoteFilter = "";
+
+  PianiListWithoutFilter:any = [];
 
     ngOnInit(): void {
       this.refreshPianiList();
@@ -22,9 +28,41 @@ export class ShowPianiComponent implements OnInit{
     this.service.getPianiList().subscribe(data => {
       this.PianiList = data;
       console.log("PianiList", this.PianiList);
-     /* this.EserciziListWithoutFilter = data;*/
+      this.PianiListWithoutFilter = data;
     });
   }
+
+  sortResult(prop: any, asc: any) {
+    this.PianiList = this.PianiListWithoutFilter.sort(function (a: any, b: any) {
+      if (asc) {
+        return (a[prop] > b[prop]) ? 1 : ((a[prop] < b[prop]) ? -1 : 0);
+      }
+      else {
+        return (b[prop] > a[prop]) ? 1 : ((b[prop] < a[prop]) ? -1 : 0);
+      }
+    });
+  }
+
+  FilterFn() {
+    var PianiIdFilter = this.PianiIdFilter;
+    var NomePianoFilter = this.NomePianoFilter;
+    var DescrizioneFilter = this.DescrizioneFilter;
+    var NoteFilter = this.NoteFilter;
+
+    this.PianiList = this.PianiListWithoutFilter.filter(
+      function (el: any) {
+        return el.pianoId.toString().toLowerCase().includes(
+          PianiIdFilter.toString().trim().toLowerCase()
+        ) && el.nomePiano.toString().toLowerCase().includes(
+          NomePianoFilter.toString().trim().toLowerCase()
+        ) && el.descrizione.toString().toLowerCase().includes(
+          DescrizioneFilter.toString().trim().toLowerCase())
+          && el.note.toString().toLowerCase().includes(
+            NoteFilter.toString().trim().toLowerCase())
+      }
+    );
+  }
+
 }
 
 
