428994c377e3b84301a28882d0449cd93cbbcbcb
Annibale Menolascina <annibale.menolascina@gmail.com>
2025-09-29 12:19:32 +0200

    Gesiter validation feedback

--- vitoswimpt.client/src/app/components/user/register-user/register-user.component.ts
+++ vitoswimpt.client/src/app/components/user/register-user/register-user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ApiserviceService } from '../../../apiservice.service';
 import { Router } from '@angular/router';
 
@@ -8,13 +8,24 @@ import { Router } from '@angular/router';
   templateUrl: './register-user.component.html',
   styleUrl: './register-user.component.css'
 })
-export class RegisterUserComponent {
+export class RegisterUserComponent implements OnInit {
   reg_firstname =  "";
   reg_lastname = "";
   reg_email = "";
   reg_password = "";
   req_password_confirm = "";
+
+  //array of validations coming from server
+  validationErrors!: { ['firstname']: undefined, ['lastname']: undefined, ['email']: undefined, ['password']: undefined, ['confirmpassword']: undefined };
+
+
+
+
+  submitted = false;
   constructor(private service: ApiserviceService, private router: Router) { }
+  ngOnInit(): void {
+    this.validationErrors = { ['firstname']: undefined, ['lastname']: undefined, ['email']: undefined, ['password']: undefined, ['confirmpassword']: undefined };
+    }
 
 
   register() { 
@@ -24,8 +35,13 @@ export class RegisterUserComponent {
       "email": this.reg_email,
       "password": this.reg_password,
       "confirmPassword": this.req_password_confirm
-  };
+    };
+
+    this.submitted = true;
+
     let token = this.service.register(credentials).subscribe((data: any) => {
+      //let checkconsistency = typeof (this.validationErrors) != 'undefined';
+      //console.log('Validation Error is different from undefined: ' + checkconsistency);
       let user = data;
       console.log(user.value);
       alert('User registered! Wait for activation email');
@@ -35,7 +51,9 @@ export class RegisterUserComponent {
       //this.loading = false;
       //window.location.reload();   
     }, error => {
-      alert(error.error.title + ' : ' + error.error.detail);
+      this.validationErrors = error.error.errors;
+      console.log(this.validationErrors);
+      //alert(error.error.title + ' : ' + error.error.detail);
     });
   }
 
