bf9058e46c0097ab0e174e89a3e7bfdfa9aa7d15
Annibale Menolascina <annibale.menolascina@gmail.com>
2025-10-10 15:40:14 +0200

    logout and dataservice

---
+++ vitoswimpt.client/src/app/components/user/logout-user/logout-user.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { DataService } from '../../../data.service';
+
+@Component({
+  selector: 'app-logout-user',
+  standalone: false,
+  templateUrl: './logout-user.component.html',
+  styleUrl: './logout-user.component.css'
+})
+export class LogoutUserComponent {
+
+  constructor(private router: Router, private route: ActivatedRoute, private dataService: DataService) {
+  }
+
+  logout() {
+    debugger;
+    sessionStorage.clear();
+    this.dataService.updateData(false);
+    /*  this.router.navigate(['']);*/
+    /*    window.location.reload();*/
+    /* this.router.onSameUrlNavigation = 'reload';*/
+    if (window.location.href.indexOf('login') !== -1) {
+      window.location.reload();
+    } else {
+      /* this.router.navigate(['']);*/
+      window.location.href = '';
+    } 
+  }
+
+}
