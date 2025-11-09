835a3f8da81cfe0a3c143ac6715f64c9d70cc270
Annibale Menolascina <annibale.menolascina@gmail.com>
2025-11-09 10:05:05 +0100

    BaseEntityType and EsercizioConfig

---
+++ VitoSwimPT.Server/Infrastructure/EsercizioConfig.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Numerics;
+using VitoSwimPT.Server.Models;
+
+namespace VitoSwimPT.Server.Infrastructure
+{
+    public class EsercizioConfig: BaseEntityTypeConfiguration<Esercizio>
+    {
+        public override void Configure(EntityTypeBuilder<Esercizio> builder)
+        {
+            base.Configure(builder);
+        }
+    }
+}
